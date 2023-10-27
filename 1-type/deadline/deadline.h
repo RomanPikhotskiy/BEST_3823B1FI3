8efddbd408fd0568e897c98a4ec03e63500ce9f4
RomanPikhotskiy <romapanda5@mail.ru>
2023-10-27 15:35:25 +0300

    dedline

--- 1-type/deadline/deadline.h
+++ 1-type/deadline/deadline.h
@@ -1,17 +1,23 @@
 int get_score(short score, int start, int stop, int now) {
-    // if (now <= start){
-    //     return score ;
-    // }
-    // if (now >= stop){
-    //     return score / 2 ;
-    // }
-    // int a = (score/2) / (stop - start);
-
-    // while(now != start){
-    //     score -= a;
-    //     now -= 1 ;
-    // }
-    // return score ;
+    float scr = score;
+    float str = start;
+    float stp = stop;
+    float nw = now ;
+    if (now <= start){
+        return score ;
+    }
+    if (now >= stop + start){
+        return score / 2 ;
+    }
+    float a = (scr/2) / (stp - str);
+    
+    while(nw != str){
+        scr -= a;
+        nw -= 1 ;
+    }
+    int answer = scr;
+    if ( scr - answer > 0 ) answer += 1 ;
+    return answer ;
 
     return 0; // Not implementation
 }
--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,5 +1,21 @@
 #include <math.h>
 
 char find_bin_pattern(int number){
-    return 100;
+    int num_two = 0 , a = 1;
+    while (number != 0 ){
+        num_two += (number % 2) * a;
+        number /= 2 ; 
+        a *= 10;
+    }
+    a /= 100;
+    int k = 1, count = 0, x = num_two ;
+    while(x > 0){
+        x = num_two % k / a ;
+        k *= 10;
+        if(x == 101)
+        count += 1;
+        a /= 10;
+    }
+    return count ;
+
 }
