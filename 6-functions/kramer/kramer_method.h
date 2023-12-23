4bd0a42913839bedc463616dd9e6c90103b01077
Roman Pikhotskiy <romapanda5@mail.ru>
2023-12-23 10:08:41 +0300

    asd

--- 6-functions/kramer/kramer_method.h
+++ 6-functions/kramer/kramer_method.h
@@ -1,31 +1,69 @@
 #include <math.h>
 
 double kramer_method(int* matrix, int* vector, int size){
-    double summ = 0 ;
-    double ns = 0 ;
-    int a = *matrix;
-    matrix ++ ;
-    int b = *matrix;
-    matrix ++ ;
-    int c = *matrix;
+    float vec[size];
     if(size == 3 ){
-        ns = -1;
-        for (int i = 0 ; i < 10 ; i ++ ){
-            for (int j = 0 ; j < 10 ; j ++ ){
-                for (int k = 0 ; k < 10 ; k ++ )
-                {
-                    if (a * i + b * j + c * k == *vector ){
-                        ns += sqrt(i) + sqrt(j) + sqrt(k); 
-                    }
-                }
-            }
+        for(int i = 0 ; i < size; i ++){
+            vec[i] = vector[i];
         }
-        if (ns == -1 ){
-            return -1 ;
+        float c = 0 , k = 0, g1 = 1, g2 = 1 ;
+        c = matrix[3];
+        k = matrix[6];
+        matrix[4] -= matrix[1] * c;
+        matrix[5] -= matrix[2] * c;
+        matrix[7] -= matrix[1] * k;
+        matrix[8] -= matrix[2] * k;
+        vec[1] -= vec[0] * c;
+        vec[2] -= vec[0] * k;
+        
+        if(matrix[4] * matrix[5] * matrix[8] * matrix[7] == 0){
+            return -1;
         }
-        summ += ns ;
-        matrix ++ ;
-        vector ++ ;
+        
+        for(int i = 0 ; i < 3; i ++ ){
+            
+        }
+        
+        if(matrix[4] != 0){
+            float s = 1.0 / matrix[4];
+            
+            matrix[4] = 1;
+            matrix[5] *= s;
+            vec[1] *= s;
+            
+            c = 0 ;
+            k = 0 ;
+            g1 = 1 ;
+            g2 = 1 ;
+            c = matrix[1];
+            k = matrix[7];
+            matrix[2] -= matrix[5] * c;
+            matrix[8] -= matrix[5] * k;
+            vec[0] -= vec[1] * c;
+            vec[2] -= vec[1] * k;
+        }
+        for(int i = 0 ; i < 3; i ++ ){
+            
+        }
+        
+        if(matrix[8] != 0){
+            int p = 8;
+            float s = 1.0 / matrix[p];
+            
+            matrix[p] = 1;
+            vec[2] *= s;
+            c = 0 ;
+            k = 0 ;
+            g1 = 1;
+            g2 = 1;
+            
+            c = matrix[5];
+            k = matrix[2];
+    
+            vec[1] -= vec[2] * c;
+            vec[0] -= vec[2] * k;
+        
+        }
     }
-    return summ ;
+    return (sqrt(vec[0]) + sqrt(vec[1]) + sqrt(vec[2]));
 }
