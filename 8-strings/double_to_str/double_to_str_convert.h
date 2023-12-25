4eb0a85ab72cd8903cfd2fd04fd3d1381b9950e2
Roman Pikhotskiy <romapanda5@mail.ru>
2023-12-25 13:08:35 +0300

    asd

--- 8-strings/double_to_str/double_to_str_convert.h
+++ 8-strings/double_to_str/double_to_str_convert.h
@@ -1,6 +1,70 @@
 #include <math.h>
 #include <string.h>
+#include <stdlib.h>
 
 char* convert(double number){
-    return NULL;
+    int u = 0;
+    if(number < 0){
+        u = 1;
+        number = -number;
+    }
+    int a = 10;
+    int p = 1;
+    while(p != 0 ){
+        p = number / a ;
+        a *= 10;
+    }
+    a /= 10;
+    a /= 10;
+    int k = number;
+    int l = -1;
+    while(1){
+        l += 1;
+        k = number;
+        if(k % 10 == 9){
+            k += 1.0;
+        }
+
+        if(k * 1.0 == number || l == 5){
+            break;
+        }
+        number *= 10.0;
+        
+    }
+    while(k % 10 == 0){
+        k /= 10;
+        l -= 1;
+    }
+    l = pow(10, l);
+    char * str = (char *) malloc(256);
+    while(k){
+        
+        int j = k % 10;
+
+        k /= 10;
+        char o = j + '0';
+        char i[256];
+        i[0] = o;
+        strcat(str, i);
+        l /= 10;
+        if(l == 1){
+            o = '.';
+            i[0] = o;
+            strcat(str, i);
+        }
+        
+        
+    }
+    if(u == 1){
+        str[strlen(str)] = '-';
+    }
+    for(int i = 0; i < strlen(str)/2; i ++){
+        char r = str[strlen(str) - 1 - i];
+        str[strlen(str) - 1 - i] = str[i];
+        str[i] = r;
+    }
+    if(str[strlen(str) - 1] == '0'){
+        str[strlen(str) - 1] = ' ';
+    }
+    return str;
 }
--- 8-strings/palindrome/find_palindrom.h
+++ 8-strings/palindrome/find_palindrom.h
@@ -2,5 +2,22 @@
 #include <string.h>
 
 int is_palindrom(char text[]){
-    return -100;
+    int j = 0;
+    int k = 0;
+    while(text[strlen(text) - j - 1] == (int)' '){
+        j += 1;
+    }
+    for(int i = 0 ; i < strlen(text) / 2; i ++){
+        while(((int)text[i + k] < 65 || (int)text[i + k] > 90) && ((int)text[i + k] < 97 || (int)text[i + k] > 122) )
+        {
+            k ++ ;
+        }
+        while(((int)text[strlen(text) - 1 - i - j] < 65 || (int)text[strlen(text) - 1 - i - j] > 90) && ((int)text[strlen(text) - 1 - i - j] < 97 || (int)text[strlen(text) - 1 - i - j] > 122))
+        {
+            j ++ ;
+        }
+        if((int)text[i + k] != (int)text[strlen(text) - 1 - i - j] && (int)text[i + k] != (int)text[strlen(text) - 1 - i - j] - 32 && (int)text[i + k] != (int)text[strlen(text) - 1 - i - j] + 32)
+        return 0;
+    }
+    return 1;
 }
