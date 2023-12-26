75a19515293bf4c67428af03227b40c63998c6f4
Roman Pikhotskiy <romapanda5@mail.ru>
2023-12-27 00:11:25 +0300

    asd

--- 9-files/fileinfo/file_info.h
+++ 9-files/fileinfo/file_info.h
@@ -2,6 +2,7 @@
 #include <string.h>
 
 unsigned int get_fileinfo(char filename[], unsigned int* abzac_num, unsigned int* num_word){
-
+    FILE * fp = fopen(filename, 'r');
+    fscanf(fp, "%s%d", str, &a);
     return 0;
 }
