2b25e7ef24e5fa3ff851cb4ab448265176fdb4d6
Roman Pikhotskiy <romapanda5@mail.ru>
2023-12-27 01:25:22 +0300

    asd

--- 1-type/byte/byte.h
+++ 1-type/byte/byte.h
@@ -1,3 +1,9 @@
 unsigned char max_byte(unsigned short number) {
-    return 0; // Not implementation
+    unsigned short a, b;
+    a = number & 255;
+    b = (number & 65280) / 256;
+    if (a > b)
+    return a;
+    else
+    return b;
 }
--- 3-loop/integral/integral.h
+++ 3-loop/integral/integral.h
@@ -1,6 +1,12 @@
 #include <math.h>
 
 double integral(double a, double b, double c, double left, double right){
-    
-    return 0.0;
+    double y0 = (b * b) / 4 / a - (b * b) / 2 / a + c;
+    double d = b * b - 4 * a *c;
+    double x1 = (-b + sqrt(d)) / 2 / a;
+    double x2 = (-b - sqrt(d)) / 2 / a;
+    if (((left == x1) & (right == x2) & (y0 < 0)) || ((left == x2) & (right == x1) & (y0 < 0)))
+    return -1;
+    double Integral = fabs((a / 3) * pow (left, 3) + (b / 2) * pow(left, 2) + c * left - ((a / 3)* pow(right, 3) + (b / 2) * pow(right,2)+c * right));
+    return Integral;
 }
