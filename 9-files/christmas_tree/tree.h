c32b719071671fc2453e7bb74bf7222ea89328cb
Roman Pikhotskiy <romapanda5@mail.ru>
2023-12-27 13:00:06 +0300

    asd

--- 9-files/christmas_tree/tree.h
+++ 9-files/christmas_tree/tree.h
@@ -3,61 +3,61 @@
 #include <stdio.h>
 
 char* tree_writer(int size){
-    FILE * fp = fopen("tadatadatada.txt", "w");
-    char a[256] ;
-    int c = 0 ;
-    for(int i = 0 ; i < size - 1; i ++){
-        a[c] = ' ';
-        c += 1;
-    }
-    a[c] = '*';
-    c += 1;
-    for(int i = 0 ; i < size - 1; i ++){
-        a[c] = ' ';
-        c += 1;
-    }
-    a[c] = '\n';
-    c ++ ;
-    for(int i = 0 ; i < size - 1; i ++){
-        a[c] = ' ';
-        c += 1;
-    }
-   a[c] = '^' ;
-   c ++ ;
-   a[c] = '\n';
-   c ++ ;
-    for(int i = 0 ; i < size - 1 ; i ++ ){
-        for(int j = 0 ; j < size - i - 2 ; j ++){
-            a[c] = ' ';
-            c += 1;
-        }
-        for(int j = 0 ; j < i + 1 ; j ++){
-            a[c] = '^';
-            c ++ ;
-        }
-        a[c] = '|';
-        c += 1;
-        for(int j = 0 ; j < i + 1 ; j ++){
-            a[c] = '^';
-            c ++ ;
-        }
-        for(int j = 0 ; j < size - i - 2 ; j ++){
-            a[c] = ' ';
-            c += 1;
-        }
-        a[c] = '\n';
-        c += 1;
-    }
-    for(int i = 0 ; i < size - 2 ; i ++){
-        a[c] = ' ';
-        c += 1;
-    }
-    a[c] = '_';
-    c ++ ;
-    a[c] = ' ';
-    c ++ ;
-    a[c] = '_';
+//     FILE * fp = fopen("tadatadatada.txt", "w");
+//     char a[256] ;
+//     int c = 0 ;
+//     for(int i = 0 ; i < size - 1; i ++){
+//         a[c] = ' ';
+//         c += 1;
+//     }
+//     a[c] = '*';
+//     c += 1;
+//     for(int i = 0 ; i < size - 1; i ++){
+//         a[c] = ' ';
+//         c += 1;
+//     }
+//     a[c] = '\n';
+//     c ++ ;
+//     for(int i = 0 ; i < size - 1; i ++){
+//         a[c] = ' ';
+//         c += 1;
+//     }
+//    a[c] = '^' ;
+//    c ++ ;
+//    a[c] = '\n';
+//    c ++ ;
+//     for(int i = 0 ; i < size - 1 ; i ++ ){
+//         for(int j = 0 ; j < size - i - 2 ; j ++){
+//             a[c] = ' ';
+//             c += 1;
+//         }
+//         for(int j = 0 ; j < i + 1 ; j ++){
+//             a[c] = '^';
+//             c ++ ;
+//         }
+//         a[c] = '|';
+//         c += 1;
+//         for(int j = 0 ; j < i + 1 ; j ++){
+//             a[c] = '^';
+//             c ++ ;
+//         }
+//         for(int j = 0 ; j < size - i - 2 ; j ++){
+//             a[c] = ' ';
+//             c += 1;
+//         }
+//         a[c] = '\n';
+//         c += 1;
+//     }
+//     for(int i = 0 ; i < size - 2 ; i ++){
+//         a[c] = ' ';
+//         c += 1;
+//     }
+//     a[c] = '_';
+//     c ++ ;
+//     a[c] = ' ';
+//     c ++ ;
+//     a[c] = '_';
     
-    fputs(a, fp);
-    return a;
+//     fputs(a, fp);
+//     return a;
 }
--- 9-files/fileinfo/file_info.h
+++ 9-files/fileinfo/file_info.h
@@ -2,7 +2,7 @@
 #include <string.h>
 
 unsigned int get_fileinfo(char filename[], unsigned int* abzac_num, unsigned int* num_word){
-    FILE * fp = fopen(filename, 'r');
-    fscanf(fp, "%s%d", str, &a);
-    return 0;
+    // FILE * fp = fopen(filename, 'r');
+    // fscanf(fp, "%s%d", str, &a);
+    // return 0;
 }
