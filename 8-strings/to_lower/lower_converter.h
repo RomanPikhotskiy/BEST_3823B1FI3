530d4d6e24430f594f3cb93c1aded925859f6bbf
Roman Pikhotskiy <romapanda5@mail.ru>
2023-12-27 13:14:33 +0300

    asd

--- 8-strings/to_lower/lower_converter.h
+++ 8-strings/to_lower/lower_converter.h
@@ -2,5 +2,11 @@
 #include <string.h>
 
 void lower(char* str){
-
+    int i = 0 ;
+    while(str[i]){
+        if ((int)str[i] >= (int)'A' && (int)str[i] <= (int)'Z'){
+            str[i] += ' ';
+        }
+        i ++ ;
+}
 }
